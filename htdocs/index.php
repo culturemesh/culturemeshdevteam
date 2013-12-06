d2d44e1ce12ca0ae44262c9cd7288bdfc0cfb6d1
PublicFriend <inottage@yahoo.com>
2013-12-06 03:15:29 -0800

    finished cosmetics excepting a few issues

--- htdocs/index.php
+++ htdocs/index.php
@@ -20,14 +20,34 @@
 				include "header.php";
 			?>
 			<div id="stage-area">
-				<p>Connecting the World's Diasporas</p>
-				<p> Search bar n things </p>
+				<div id="stage-content">
+					<h3 id="stage-title">Connecting the World's Diasporas</h3>
+					<div id="search-bar">
+						<div id="search-floater">
+						<div id="search-content">
+						<form id="search-form">
+							<input type="text" class="stage-input" value="Find People who"></input>
+							<input type="text" class="stage-input" value="Near"></input>
+							<input type="submit" class="stage-button" value="Search"></input>
+						</form>
+						</div>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div id="bottom-section">
-				<div id="vision">
+				<div id="vision" class="bottom-div">
 					<h4>Our Vision</h4>
+					<p> Millions of people live, work, and
+					play outside of their home towns, provinces,
+					states, and countries.
+					</p>
+					<p> At CultureMesh, we're building networks to
+					match these real-world dynamics and knit the
+					diverse fabrics of our world together.
+					</p>
 				</div>
-				<div id="process">
+				<div id="process" class="bottom-div">
 					<h4>How it works</h4>
 					<ol>
 						<li>
@@ -49,9 +69,10 @@
 						</li>
 					</ol>
 				</div>
-				<div id="pop-networks">
+				<div id="pop-networks" class="bottom-div">
 					<h4>Popular Networks</h4>
 				</div>
+				<div class="clear"></div>
 			</div>
 			<?php
 				include "footer.php";
