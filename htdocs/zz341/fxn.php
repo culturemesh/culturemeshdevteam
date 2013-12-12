a09a7ab2a1c4cd351fdee216e857aeb9911c24df
PublicFriend <inottage@yahoo.com>
2013-12-11 19:39:53 -0800

    made it easier to switch between dev environments

--- htdocs/zz341/fxn.php
+++ htdocs/zz341/fxn.php
@@ -8,10 +8,19 @@ define("SUPPORT_EMAIL", "");
 define("WEBSITE_BY_URL", "http://www.kostocoastdev.com");
 
 define("JS_HOLDER_64x64", "http://www.kostocoastdev.com/clients/hosted/js/holder.js/64x64");
-define("DB_SERVER", "localhost");
-define("DB_USER", "culturp7");
-define("DB_PASS", "d4T48@$3");
-define("DB_NAME", "culturp7_ktc");
+
+if ( file_exists('../../localdbconn.php'))
+{
+    include  "../../localdbconn.php";
+}
+else
+{
+    echo("using other thing");
+    define("DB_SERVER", "localhost");
+    define("DB_USER", "culturp7");
+    define("DB_PASS", "d4T48@$3");
+    define("DB_NAME", "culturp7_ktc");
+}
 
 function getDBConnection(){
     $conn = new mysqli(DB_SERVER,DB_USER,DB_PASS, DB_NAME);
