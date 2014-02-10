82da4557264a46b25d1f19d46362290fffa2394e
mikos <jenki221@gmail.com>
2014-02-10 02:15:27 -0800

    changed fxn db constants back to production values

--- htdocs/zz341/fxn.php
+++ htdocs/zz341/fxn.php
@@ -8,7 +8,7 @@ define("SUPPORT_EMAIL", "");
 define("WEBSITE_BY_URL", "http://www.kostocoastdev.com");
 
 define("JS_HOLDER_64x64", "http://www.kostocoastdev.com/clients/hosted/js/holder.js/64x64");
-/*
+
 if ( file_exists('../localdbconn.php'))
 {
     include  "../localdbconn.php";
@@ -20,14 +20,6 @@ else
     include "../../../abcd123.php";
     define("DB_NAME", "culturp7_ktc");
 }
-*/
-
-/****/
-define("DB_SERVER", "localhost");
-define("DB_USER", "culturp7");
-define("DB_NAME", "culturp7_ktc");
-define("DB_PASS", "GoRoop2013!");
-/***/
 function getDBConnection(){
     $conn = new mysqli(DB_SERVER,DB_USER,DB_PASS, DB_NAME);
     return $conn;
