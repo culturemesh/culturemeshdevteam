325bdb68089bfc1c8457891ce116c4df326f17f1
i-nottage <inottage@yahoo.com>
2015-07-31 04:14:29 -0600

    added environment to press page

--- htdocs/press.php
+++ htdocs/press.php
@@ -1,5 +1,8 @@
 <?php
     include_once 'zz341/fxn.php';
+    include 'environment.php';
+    $cm = new Environment();
+
     $ppre = "press_pg";
 //    include 'page_tpl.php';
     include 'reg_page_tpl.php';
