ce31be666de7a8b99ee3b0b92e043c4af2912c5d
i-nottage <inottage@yahoo.com>
2015-11-12 20:54:31 -0800

    deleted old code around post deletion

--- htdocs/network_post_delete.php
+++ htdocs/network_post_delete.php
@@ -17,37 +17,15 @@ $response = array(
 // if pid is set, we can continue
 if (isset($_POST['pid']) && isset($_POST['nid']) && isset($_POST['replies'])) {
 
-	// important includes
-	/*
-	include_once("data/dal_post.php");
-	include_once("data/dal_query_handler.php");
-	include_once("html_builder.php");
-	 */
-
 	// stuff
 	$pid = $_POST['pid'];
 	$nid = $_POST['nid'];
 	$uid = $_SESSION['uid'];
 	$nrs = $_POST['replies'];
 
-	/*
-	// if there are no replies, check db to see if we
-	// can delete the entire post 
-	$con = QueryHandler::getDBConnection();
-
-	// check count in database
-	$replies = Post::getRepliesByParentId($pid, $con);
-
-	$nrs = count($replies);
-	 */
 	$dal = new \dal\DAL($cm->getConnection());
 	$dal->loadFiles();
 	$do2db = new \dal\Do2Db();
-
-	/*
-	$post = new \dobj\Post();
-	$post->id = (int) $_POST['pid'];
-	 */
 
 	$site_user = \dobj\User::createFromId($uid, $dal, $do2db);
 	$post = \dobj\Post::createFromId((int) $_POST['pid'], $dal, $do2db);
@@ -58,11 +36,8 @@ if (isset($_POST['pid']) && isset($_POST['nid']) && isset($_POST['replies'])) {
 	// free we are to destroy the post
 	if ( $nrs == 0 ) {
 
-		//if(Post::deletePost($_POST['pid'], $con)) {
-
 		if($post->delete($dal, $do2db)) {
 			// close connection
-			//mysqli_close($con);
 			$cm->closeConnection();
 
 			if (isset($_POST['NOJS'])) {
@@ -85,7 +60,6 @@ if (isset($_POST['pid']) && isset($_POST['nid']) && isset($_POST['replies'])) {
 	}
 	else {
 		// just wipe the post
-		//if (Post::wipePost($pid, $con)) {
 		if ($post->wipe($dal, $do2db)) {
 			if (isset($_POST['NOJS'])) {
 				//mysqli_close($con);
@@ -93,16 +67,6 @@ if (isset($_POST['pid']) && isset($_POST['nid']) && isset($_POST['replies'])) {
 				header("Location: network.php?id={$_POST['nid']}&dp=true");
 			}
 			else {
-				/*
-				// now get post and get replies for replacing with ajax
-				$replies = NULL;
-				$post = Post::getPostById($pid, $con);
-
-				$response['error'] = 0;
-				$response['status'] = 'wiped';
-				$response['html'] = HTMLBuilder::displayPost($post, $replies);
-				 */
-
 				// create network
 				$network = new \dobj\Network();
 				$network->id = (int) $nid;
@@ -124,14 +88,13 @@ if (isset($_POST['pid']) && isset($_POST['nid']) && isset($_POST['replies'])) {
 				$response['status'] = 'wiped';
 				$response['html'] = $html;
 
-				//mysqli_close($con);
 				echo json_encode($response);
 			}
 		}
 		else {
 
-				//mysqli_close($con);
-				$cm->closeConnection();
+			$cm->closeConnection();
+
 			if (isset($_POST['NOJS'])) {
 				header("Location: network.php?id={$_POST['nid']}&dp=false");
 			}
