5b1a8a36aafcef8c7217d9dbc6a002a99e6610be
i-nottage <inottage@yahoo.com>
2016-05-31 00:46:43 -0700

    now using sass

---
+++ htdocs/devlab/namesearchspeedtest.php
@@ -0,0 +1,13 @@
+<?php
+	include("../environment.php");
+	$cm = new Environment();
+
+	// Basic test for mobile device
+	$mobile_detect = new \misc\MobileDetect();
+
+	$page_loader = new \misc\PageLoader($cm, $mobile_detect);
+	echo $page_loader->generate('devlab' . $cm->ds . 'templates' . $cm->ds .'namesearchspeedtest.html', array(
+		'vars' => $cm->getVars(),
+		'mobile' => $mobile_detect->isMobile()
+	));
+?>
